e66cc5438440e1e557bc6c5b51e6813b87aa5d5a
bidlia <loganbidell@gmail.com>
2026-08-25 20:25:52 -0700

    fix; it's an addiction. Corrected build tint representation from 0xAABBCC to #AABBCC, and italicized command descriptions on the command usage help embed.

--- src/commands/utility/help/usage.ts
+++ src/commands/utility/help/usage.ts
@@ -5,15 +5,15 @@ export function parseUsage(command: Command): string[] {
   const usagePermutations: string[] = [];
 
   usagePermutations.push(
-    `\`/${command.data.name}\` ${command.data.description}`,
+    `\`/${command.data.name}\` *${command.data.description}*`,
   );
 
   if (command.usage) {
     rawData.options
       ?.map((opt) =>
         command.usage![opt.name].required
-          ? `\`/${command.data.name} ${opt.name}:<${command.usage![opt.name].value}>\` ${opt.description}`
-          : `\`/${command.data.name} ${opt.name}:[${command.usage![opt.name].value}]\` ${opt.description}`,
+          ? `\`/${command.data.name} ${opt.name}:<${command.usage![opt.name].value}>\` *${opt.description}*`
+          : `\`/${command.data.name} ${opt.name}:[${command.usage![opt.name].value}]\` *${opt.description}*`,
       )
       .forEach((opt) => usagePermutations.push(opt));
   }
